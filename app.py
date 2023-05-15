f7bbdddf2969506cb24438c40676ccfdd3f82dc4
Luis M. Aviles A <lumaaviles0@gmail.com>
2023-05-15 17:01:59 -0500

    add functionality of the neighboring_neighborhood function.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv
 import os
 
-from geoutilities import connection_gmap, address_coordinates
-from geoutilities import coordinates_neighborhood, neighboring_neighborhood
+from geoutilities import connection_gmap, address_to_coordinates
+from geoutilities import coordinates_to_neighborhood, neighboring_neighborhood
 
 load_dotenv()
 
@@ -11,6 +11,8 @@ if __name__ == "__main__":
     gmaps = connection_gmap(key)
 
     address = '1300 SE Stark Street, Portland, OR 97214'
-    latitude, longitude = address_coordinates(address, gmaps)
+    latitude, longitude = address_to_coordinates(address, gmaps)
 
-    print(latitude, longitude)
+    neighborhood = coordinates_to_neighborhood(latitude, longitude)
+
+    neighborhood_2 = neighboring_neighborhood(latitude, longitude, neighborhood)
--- geoutilities.py
+++ geoutilities.py
@@ -1,17 +1,29 @@
 import googlemaps
+import requests
 
 def connection_gmap(key):
     return googlemaps.Client(key=key)
 
-def address_coordinates(address, gmaps):
+def address_to_coordinates(address, gmaps):
     geocode_result = gmaps.geocode(address)
     lat = geocode_result[0]['geometry']['location']['lat']
     lng = geocode_result[0]['geometry']['location']['lng']
 
     return (lat, lng)
 
-def coordinates_neighborhood(lat, lng):
-    pass
+def coordinates_to_neighborhood(latitude, longitude):
+    url = 'https://www.portlandmaps.com/arcgis/rest/services/Public/COP_OpenData/MapServer/125/query'
+    parameters = {
+        'where': '1=1',
+        'geometry': f"{'x':{longitude},'y':{latitude}}"
+    }
+    
+    response = requests.get(url, params=parameters)
+    print(response)
 
-def neighboring_neighborhood(neighboring, neighborhood='', move=100):
-    pass
+def neighboring_neighborhood(latitude, longitude, neighboring):
+    neighborhood = coordinates_to_neighborhood(latitude, longitude)
+    if(neighborhood != neighboring):
+        return neighborhood
+    else:
+        return neighboring_neighborhood(latitude+100, longitude+100, neighboring)
